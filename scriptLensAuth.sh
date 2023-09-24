68ed84f5756773af1229fa22e923eedb5cf38683
starit <janyucheng@gmail.com>
2023-09-23 21:07:48 -0400

    update deploy scripts for convinience

---
+++ scriptLensAuth.sh
@@ -0,0 +1,1 @@
+forge script script/DeployLensAuthHook.s.sol  --broadcast --fork-url http://localhost:8545 --code-size-limit 300000 -vvvvv
---
+++ scriptLensPost.sh
@@ -0,0 +1,1 @@
+forge script script/DeployLensPostHook.s.sol  --broadcast --fork-url http://localhost:8545 --code-size-limit 300000 -vvvvv
